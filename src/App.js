98b83d7351cd694e52006e4e09b4bca00ac9889b
Tring-santhoshkumar <santhoshkumar.a@tringapps.com>
2025-02-16 10:43:44 +0530

    App Component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './FormStyleSheet.css'
+import './FetchDataStyleSheet.css'
+import './CrudStyleSheet.css'
+import Form from './Form'
+import FetchData from './FetchData'
+import Cards from './Cards'
+import Crud from './Crud'
+
+function App() {
+  return (
+     <>
+     <Form />
+     <FetchData />
+     <Cards />
+     <Crud />
+     </>
+  );
+}
+
+export default App;
