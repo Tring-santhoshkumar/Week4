612702d2aba04c1787b1a36e4557da21815012ce
Tring-santhoshkumar <santhoshkumar.a@tringapps.com>
2025-02-16 22:36:40 +0530

    Login Page - Updation

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { UserProfile } from './UserProfile';
 
 const Login = () => {
 
@@ -13,22 +14,30 @@ const Login = () => {
         setInputData({...inputData,[e.target.name]:e.target.value});
     }
 
-    const submitInput = (e) => {                                  //Getting from localstorage to login
+    const {login} = useContext(UserProfile);
+
+    const submitInput = (e) => {                                  
         e.preventDefault();
-        const registeredUser = JSON.parse(localStorage.getItem("userRegister"));
-        if(registeredUser.email == inputData.email && registeredUser.password == inputData.password){
-            alert("Login Successfull!!!");
-            navigate('/userHome');
+        if(login(inputData.email,inputData.password)){
+            alert("Login Successsfull!!!");
+            // navigate(`/userHome/${inputData.email}`);
+            navigate("/UserHome");
+        }
+        else{
+            alert("Invalid Details,Please login again!");
         }
       }
+
   return (
-    <div>
-        <h1>Login Page</h1>                                         {/*Login form*/}
-        <form onSubmit={submitInput}>
-            <input type='email' name='email' placeholder='Enter email' value={inputData.email} onChange={addInput} required pattern='.+@tringapps.com' title='Ex : santhoshkumar.a@tringapps.com'/>
-            <input type='password' name='password' placeholder='Enter password' value={inputData.password} onChange={addInput} required pattern='(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{5-}' title='Ex : Admin@123'/>
-            <button type='submit'>Submit</button>
-        </form>
+    <div className='registerContainer'>
+        <div className='registerPopup'>
+            <h1>Login Page</h1>                                         {/*Login form*/}
+            <form onSubmit={submitInput}>
+                <input type='email' name='email' placeholder='Enter email' value={inputData.email} onChange={addInput} required pattern='.+@tringapps.com' title='Ex : santhoshkumar.a@tringapps.com'/>
+                <input type='password' name='password' placeholder='Enter password' value={inputData.password} onChange={addInput} required pattern='(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{5-}' title='Ex : Admin@123'/>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     </div>
   )
 }
