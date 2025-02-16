365c4a6edb8b5ec8844de631fc5a7362c637f7fa
Tring-santhoshkumar <santhoshkumar.a@tringapps.com>
2025-02-16 22:38:43 +0530

    App component - updation

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import Register from './Register';
 import Login from './Login';
 import UserHome from './UserHome';
 import Update from './Update';
-
+import { UserProvider } from './UserProfile'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/userHome' element={<UserHome />} />
-      <Route path='/update' element={<Update />} /> */
-     </Routes>
-   </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/userHome' element={<UserHome />} />
+        <Route path='/update' element={<Update />} /> */
+      </Routes>
+    </BrowserRouter>
+   </UserProvider>
   );
 }
 
