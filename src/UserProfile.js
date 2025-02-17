b66564b1e75bc4419e98d34a552143a60911dd00
Tring-santhoshkumar <santhoshkumar.a@tringapps.com>
2025-02-17 18:53:47 +0530

    UserProfile useContext - Updation

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -4,49 +4,55 @@ export const UserProfile = createContext();
 
 export const UserProvider = ({children}) => {
 
-    const [currentUserData, setCurrentUserData] = useState();
+    const [currentUserData, setCurrentUserData] = useState({name : null, email : null});
+
+    const [ editPersonaKey, setEditPersonaKey ] = useState(null);
 
     const [personas, setPersonas] = useState([]);
 
-    useEffect(() => {
-        const savedUsers = JSON.parse(localStorage.getItem("currentUserData"));
-        const storedPersonas = JSON.parse(localStorage.getItem("personas")) || [];
-        if(savedUsers) setCurrentUserData(savedUsers);
-        setPersonas(storedPersonas);
-    }, [])
-
-    const register = (userInput) => {
-        setCurrentUserData(userInput);
-        localStorage.setItem("currentUserData", JSON.stringify(userInput));
-    }
-
-    // console.log(userData);
-
-    const login = (email, password) => {
-        const storedUser = JSON.parse(localStorage.getItem("currentUserData"));
-        if(storedUser.email == email && storedUser.password == password){
-            setCurrentUserData(storedUser);
-            return true;
-        }
-        return false;
-    };
+    const SetEditPersonaKey = (index)=>{
+        setEditPersonaKey(index); 
+      }
 
     const addPersona = (persona) => {
         const updatedPersona = [...personas,persona];
         setPersonas(updatedPersona);
-        localStorage.setItem("personas",JSON.stringify(updatedPersona));
     }
 
-    const logout = () => {
-        setCurrentUserData(null);
-        setPersonas([]);
-        localStorage.removeItem("personas");
-        localStorage.removeItem("currentUserData");
-    };
+    const deletePersona = (index) => {
+        const OldPersonas = personas;
+        OldPersonas.splice(index, 1);
+        setPersonas(OldPersonas);
+      }
+
+      
+    // const register = (userInput) => {
+    //     setCurrentUserData(userInput);
+    //     localStorage.setItem("currentUserData", JSON.stringify(userInput));
+    // }
+
+    // console.log(userData);
+
+    // const login = (email, password) => {
+    //     const storedUser = JSON.parse(localStorage.getItem("currentUserData"));
+    //     if(storedUser.email == email && storedUser.password == password){
+    //         setCurrentUserData(storedUser);
+    //         return true;
+    //     }
+    //     return false;
+    // };
+    
+
+    // const logout = () => {
+    //     setCurrentUserData(null);
+    //     setPersonas([]);
+    //     localStorage.removeItem("personas");
+    //     localStorage.removeItem("currentUserData");
+    // };
     
 
     return (
-        <UserProfile.Provider value={{currentUserData, personas , register, login, logout, addPersona}}>
+        <UserProfile.Provider value={{currentUserData, setCurrentUserData, personas, setPersonas , addPersona, editPersonaKey, SetEditPersonaKey, deletePersona}}>
             {children}
         </UserProfile.Provider>
     );
